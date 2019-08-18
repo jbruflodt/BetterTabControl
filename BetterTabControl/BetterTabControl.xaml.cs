aadead43311cfb6a0b2785335e33d5a6920adf5c
Jacob Bruflodt <bruflodtj@gmail.com>
2019-08-18 15:27:08 -0500

    attempting to implement a way to scroll through tabs when they overflow the panel

--- BetterTabControl/BetterTabControl.xaml.cs
+++ BetterTabControl/BetterTabControl.xaml.cs
@@ -1008,4 +1008,21 @@ namespace BetterTabs
             base.RemoveItem(index);
         }
     }
+    internal class TabPresenterWidthCalculator : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(values.Length < 3 || !(values[0] is double) || !(values[1] is double))
+                return double.PositiveInfinity;
+            else if(!(values[2] is double))
+                return (double)values[0] - (double)values[1];
+            else
+                return (double)values[0] - ((double)values[1] + (double)values[2]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
